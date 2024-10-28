ab14fb43990fe4aaee3196dd700a93c7dc470a55
anicosta <anicosta@brasilseg.com.br>
2024-10-28 18:03:51 -0300

    Implementa teste unitário clienteJaCadastradoNaBase

--- src/test/java/com/br/agendamento/services/ClienteServiceTest.java
+++ src/test/java/com/br/agendamento/services/ClienteServiceTest.java
@@ -1,17 +1,19 @@
 package com.br.agendamento.services;
 
 
+import com.br.agendamento.exceptions.ClienteCadastradoException;
 import com.br.agendamento.model.Cliente;
 import com.br.agendamento.model.dtos.CadastraClienteDTO;
 import com.br.agendamento.repository.UsuarioRepository;
 import com.br.agendamento.service.ClienteService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
  class ClienteServiceTest {
@@ -23,7 +25,7 @@ import org.springframework.boot.test.context.SpringBootTest;
     protected ModelMapper modelMapper;
 
     @Mock
-    protected UsuarioRepository UsuarioRepository;
+    protected UsuarioRepository usuarioRepository;
 
 
     @Test
@@ -44,11 +46,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 
         Cliente clienteCadastrado = clienteService.cadastraCliente(clienteDTO);
 
-        Assertions.assertNotNull(clienteCadastrado);
-        Assertions.assertEquals(clienteDTO.getNome(), clienteCadastrado.getNome());
-        Assertions.assertEquals(clienteDTO.getEmail(), clienteCadastrado.getEmail());
-        Assertions.assertEquals(clienteDTO.getCodigoPessoa(), clienteCadastrado.getCodigoPessoa());
+        assertNotNull(clienteCadastrado);
+        assertEquals(clienteDTO.getNome(), clienteCadastrado.getNome());
+        assertEquals(clienteDTO.getEmail(), clienteCadastrado.getEmail());
+        assertEquals(clienteDTO.getCodigoPessoa(), clienteCadastrado.getCodigoPessoa());
 
     }
+
+    @Test
+    void clienteJaCadastradoNaBase() {
+        CadastraClienteDTO clienteDTO = new CadastraClienteDTO();
+        clienteDTO.setNome("nome");
+        clienteDTO.setEmail("teste@gmail.com");
+        clienteDTO.setCodigoPessoa("123456789");
+
+        Mockito.when(clienteService.cadastraCliente(clienteDTO)).thenThrow(new ClienteCadastradoException("Cliente já cadastrado"));
+
+        ClienteCadastradoException exception = assertThrows(
+                ClienteCadastradoException.class, () -> clienteService.cadastraCliente(clienteDTO));
+
+        assertEquals("Cliente já cadastrado", exception.getMessage());
+    }
+
+
 
 }
