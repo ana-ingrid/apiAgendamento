82b94e77399ad328a28b592ada4e2507dc77aca2
anicosta <anicosta@brasilseg.com.br>
2025-01-22 23:32:31 -0300

    Implementa anotação no DTO CadastraProfissionalDTO

--- src/main/java/com/br/agendamento/profissional/model/dtos/CadastraProfissionalDTO.java
+++ src/main/java/com/br/agendamento/profissional/model/dtos/CadastraProfissionalDTO.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.br.CPF;
 
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CadastraProfissionalDTO {
 
     @NotNull(message = "Nome obrigatório")
