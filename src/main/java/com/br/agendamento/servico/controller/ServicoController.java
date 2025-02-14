33312c0e34e84b46490d1adf4bc80266d0e5dc74
anicosta <anicosta@brasilseg.com.br>
2025-02-13 22:13:19 -0300

    Implementa classe ServicoController e ServicoService para iniciar o desenvolvimento do domínio serviço

---
+++ src/main/java/com/br/agendamento/servico/controller/ServicoController.java
@@ -0,0 +1,13 @@
+package com.br.agendamento.servico.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "sevicos")
+@AllArgsConstructor
+public class ServicoController {
+
+
+}
