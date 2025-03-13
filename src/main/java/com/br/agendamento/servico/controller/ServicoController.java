0c1eb9b5ec49e459e139319902cc946bed0f3363
ana-ingrid <ingridsantoscosta2003@gmail.com>
2025-03-12 23:30:03 -0300

    Implementa endpoints cadastraServico e consultaServico no ServicoController

--- src/main/java/com/br/agendamento/servico/controller/ServicoController.java
+++ src/main/java/com/br/agendamento/servico/controller/ServicoController.java
@@ -1,13 +1,31 @@
 package com.br.agendamento.servico.controller;
 
+import com.br.agendamento.servico.model.Servico;
+import com.br.agendamento.servico.model.dtos.CadastraServicoDTO;
+import com.br.agendamento.servico.service.ServicoService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "sevicos")
 @AllArgsConstructor
 public class ServicoController {
+
+    private ServicoService servicoService;
+
+
+    @GetMapping
+    public ResponseEntity<Servico> consultaServico(@PathVariable String nomeServico){
+        return ResponseEntity.status(200).body(servicoService.consultaServico(nomeServico, true));
+    }
+
+    @PostMapping
+    public ResponseEntity<Servico> cadastraServico(@RequestBody @Valid CadastraServicoDTO cadastraServicoDTO){
+        return ResponseEntity.status(200).body(servicoService.cadastraServico(cadastraServicoDTO));
+    }
+
 
 
 }
