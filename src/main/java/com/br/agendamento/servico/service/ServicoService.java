1857e152a80112e6ea18d02a12f78dc32ae6c331
ana-ingrid <ingridsantoscosta2003@gmail.com>
2025-03-31 23:50:03 -0300

    Implementa endpoint alteraServico na classe ServicoController

--- src/main/java/com/br/agendamento/servico/service/ServicoService.java
+++ src/main/java/com/br/agendamento/servico/service/ServicoService.java
@@ -3,6 +3,7 @@ package com.br.agendamento.servico.service;
 import com.br.agendamento.config.MensagensDeErros;
 import com.br.agendamento.servico.exceptions.ServicoNaoEncontradoException;
 import com.br.agendamento.servico.model.Servico;
+import com.br.agendamento.servico.model.dtos.AlteraServicoDTO;
 import com.br.agendamento.servico.model.dtos.CadastraServicoDTO;
 import com.br.agendamento.servico.repository.ServicoRepository;
 import com.br.agendamento.usuario.exceptions.UsuarioCadastradoException;
@@ -22,21 +23,23 @@ public class ServicoService {
     public Servico consultaServico(String nomeServico, boolean boleano) {
         Servico servico = servicoRepository.findByServico(nomeServico);
         if (Objects.isNull(servico) && boleano){
-            throw new ServicoNaoEncontradoException("Objeto servico não encontrado");
+            throw new ServicoNaoEncontradoException("Serviço não localizado");
         }
         return servico;
     }
 
     public Servico cadastraServico(CadastraServicoDTO cadastraServicoDTO){
-        if (Boolean.TRUE.equals(consultaServico(cadastraServicoDTO.getNomeServico(), true))){
-            Servico objServico = modelMapper.map(cadastraServicoDTO, Servico.class);
-            servicoRepository.save(objServico);
+        if (!Objects.isNull(consultaServico(cadastraServicoDTO.getNomeServico(),false))){
+            throw new UsuarioCadastradoException(MensagensDeErros.PROFISSIONALJACADASTRADO.getDescricao());
         }
-        throw new UsuarioCadastradoException(MensagensDeErros.PROFISSIONALJACADASTRADO.getDescricao());
+        Servico objServico = modelMapper.map(cadastraServicoDTO, Servico.class);
+        return servicoRepository.save(objServico);
     }
 
-
-
-
-
+    public Servico alteraServico(AlteraServicoDTO alteraServicoDTO) {
+        Servico servicoConsultado = consultaServico(alteraServicoDTO.getNomeServico(), true);
+        Servico objMapeado = modelMapper.map(alteraServicoDTO, Servico.class);
+        modelMapper.map(objMapeado, servicoConsultado);
+        return servicoRepository.save(objMapeado);
+    }
 }
