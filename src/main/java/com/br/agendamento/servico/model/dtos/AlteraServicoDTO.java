7349e78a2b1983df5926fd41c49afbc180e4fc28
ana-ingrid <ingridsantoscosta2003@gmail.com>
2025-04-27 11:10:07 -0300

    Novas atualização na classe AlteraServicoDTO

--- src/main/java/com/br/agendamento/servico/model/dtos/AlteraServicoDTO.java
+++ src/main/java/com/br/agendamento/servico/model/dtos/AlteraServicoDTO.java
@@ -2,7 +2,6 @@ package com.br.agendamento.servico.model.dtos;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
